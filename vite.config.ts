f9ebec56cca48fde65e5ad49a29af727d398480a
HCM24_CPL_NET_14_NguyenThanhNhan <nhanntse172412@fpt.edu.vn>
2025-02-09 18:20:36 +0700

    feat: add navigation link to Admin, Login

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,10 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ]
+  ],
+  server: {
+    port: 3000, // ✅ Chỉnh cổng tại đây
+    host: 'localhost', // (Tùy chọn) Cho phép truy cập từ localhost
+    open: true, // (Tùy chọn) Tự động mở trình duyệt khi chạy
+  }
 })
