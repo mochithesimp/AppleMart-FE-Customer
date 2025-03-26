a9ea98da8a0b4c195b88dd87d0d493a3b65ff1c0
HCM24_CPL_NET_14_NguyenThanhNhan <nhanntse172412@fpt.edu.vn>
2025-03-26 21:20:49 +0700

    fix-deploy

--- src/components/NavbarProduct/NavbarforP.tsx
+++ src/components/NavbarProduct/NavbarforP.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { IoMdSearch } from "react-icons/io";
 import { FaBell, FaCartShopping, FaTrash } from "react-icons/fa6";
 import { FaCaretDown } from "react-icons/fa";
 import noface from "../../assets/NoFace.jpg";
